96ae576188cda20db50dc86d77c49a7e77734cc4
David Saint <topstronics@gmail.com>
2019-05-16 19:48:16 +0100

    Added the jwt subject guard middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'user' => \App\Http\Middleware\UserMiddleware::class,
+        'guard' => \App\Http\Middleware\Guard::class,
     ];
 
     /**
