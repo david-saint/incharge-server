adcb71d12075991ad15f6e7e30ee5bf3dd582b4c
David Saint <topstronics@gmail.com>
2019-07-21 18:10:58 +0100

    Minor bug fix

--- app/Http/Controllers/V1/General/FaqController.php
+++ app/Http/Controllers/V1/General/FaqController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\V1\General;
 use App\Models\FaqGroup;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Resource\General\FaqGroupResource;
+use App\Http\Resources\General\FaqGroupResource;
 
 class FaqController extends Controller
 {
